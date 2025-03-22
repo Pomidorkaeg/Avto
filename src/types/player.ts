1b5306382d84a38d3df0867b085a71d4c82e8a22
Егор <egornazarko2009.09@gmail.com>
2025-03-22 10:52:44 +0300

    Initial commit

---
+++ src/types/player.ts
@@ -0,0 +1,17 @@
+
+export interface Player {
+  id: string;
+  name: string;
+  position: string;
+  number: number;
+  birthDate: string;
+  height: number;
+  weight: number;
+  nationality: string;
+  image: string;
+  matches: number;
+  goals: number;
+  assists: number;
+  yellowCards: number;
+  redCards: number;
+}
